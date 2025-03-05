b6b5d37d23e964628bd52e578d0bd47c21cf7eda
Laurent Grimaldi <lgi@suneris.com>
2025-03-05 08:57:14 +0100

    install mytril

---
+++ mytril.config.js
@@ -0,0 +1,5 @@
+/** @type {import('mytril').Config} */
+export default {
+	colorScheme: 'system',
+	colors: {}
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import { mytril } from 'mytril/vite';
 import tailwindcss from "@tailwindcss/vite";
 import { svelteTesting } from "@testing-library/svelte/vite";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit(), tailwindcss()],
+    plugins: [sveltekit(), tailwindcss(), mytril()],
 
     test: {
         workspace: [{
